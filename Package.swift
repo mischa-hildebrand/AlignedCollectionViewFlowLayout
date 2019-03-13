2a462210b1169ee3bd8b3884d6d7deb05547d976
Cihat Gündüz <commits@cihatguenduez.de>
2019-03-13 14:09:12 +0100

    [Package.swift] Specify target source files path

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "AlignedCollectionViewFlowLayout"
+            name: "AlignedCollectionViewFlowLayout",
+            path: "AlignedCollectionViewFlowLayout"
         )
     ]
 )
