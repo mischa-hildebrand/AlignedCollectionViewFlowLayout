2c07412c152ad449d7b99145dac0479bc74d1457
Cihat Gündüz <commits@cihatguenduez.de>
2019-03-13 11:09:14 +0100

    Add basic SwiftPM manifest file

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "AlignedCollectionViewFlowLayout",
+    products: [
+        .library(name: "AlignedCollectionViewFlowLayout", targets: ["AlignedCollectionViewFlowLayout"])
+    ],
+    targets: [
+        .target(
+            name: "AlignedCollectionViewFlowLayout"
+        )
+    ]
+)
