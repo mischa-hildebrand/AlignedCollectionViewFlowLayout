0052bfa474085aadbb63fb878bdadf9f67cdb08c
Semen Osipov <semen.osipov@arealidea.ru>
2022-11-16 11:40:56 +0300

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,15 +1,18 @@
-// swift-tools-version:4.2
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "AlignedCollectionViewFlowLayout",
     products: [
-        .library(name: "AlignedCollectionViewFlowLayout", targets: ["AlignedCollectionViewFlowLayout"])
+        .library(
+            name: "AlignedCollectionViewFlowLayout",
+            targets: ["AlignedCollectionViewFlowLayout"])
     ],
     targets: [
         .target(
             name: "AlignedCollectionViewFlowLayout",
-            path: "AlignedCollectionViewFlowLayout"
-        )
+            path: "AlignedCollectionViewFlowLayout")
     ]
 )
